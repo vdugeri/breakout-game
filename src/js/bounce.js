9dd46aa9ef016cc68b4b2542427d4b6f141c3d41
vdugeri <verem.dugeri@gmail.com>
2019-02-13 10:23:21 +0100

    Bounce ball off the walls

--- src/js/bounce.js
+++ src/js/bounce.js
@@ -1,16 +1,36 @@
+(function () {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  let x = canvas.width / 2;
+  let y = canvas.height - 40;
+  let dx = 2;
+  let dy = -2;
 
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const x = canvas.width / 2;
-const y = canvas.height - 40;
+  const ballRadius = 10;
 
-function draw() {
-  context.beginPath();
-  context.arc(x, y, 10, 0, Math.PI * 2);
-  context.fillStyle = '#0095dd';
-  context.fill();
-  context.closePath();
-};
+  function drawBall() {
+    context.beginPath();
+    context.arc(x, y, ballRadius, 0, Math.PI * 2);
+    context.fillStyle = '#0095dd';
+    context.fill();
+    context.closePath();
+  }
+
+  function draw() {
+    context.clearRect(0, 0 , canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
 
 
-setInterval(draw, 10);
+    if (x + dx < ballRadius || x + dx  > canvas.width - ballRadius) {
+      dx = -dx;
+    }
+
+    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+      dy = -dy;
+    }
+  }
+
+  setInterval(draw, 10);
+}());
